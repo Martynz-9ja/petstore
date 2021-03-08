40f2d6706e51f145f75fa2e78dc3af4cee8b51ae
Martins <mnonuminya@yahoo.com>
2021-03-08 09:52:00 +0100

    catch all changes

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,32 @@
 var cacheName= 'petstoreV1';
-var cacheFiles= [
-    'index.html',
-    'product.js',
-    'petstore.webmanifest',
-    'images/yarn.jpg',
-    'images/cat-litter.jpg',
-    'images/laser-pointer.jpg',
-    'images/cat-house.jpg',
-    'images/icon-512.png',
-    'images/icon-store-32.png'
-];
+// var cacheFiles= [
+//     'index.html',
+//     'product.js',
+//     'petstore.webmanifest',
+//     'images/yarn.jpg',
+//     'images/cat-litter.jpg',
+//     'images/laser-pointer.jpg',
+//     'images/cat-house.jpg',
+//     'images/icon-512.png',
+//     'images/icon-store-32.png'
+// ];
 
 self.addEventListener('install', (e)=>{
     console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
             console.log('[Service Worker] Caching all the files');
-            return cache.addAll(cacheFiles);
+            return cache.addAll([
+                'index.html',
+                'product.js',
+                'petstore.webmanifest',
+                'images/yarn.jpg',
+                'images/cat-litter.jpg',
+                'images/laser-pointer.jpg',
+                'images/cat-house.jpg',
+                'images/icon-512.png',
+                'images/icon-store-32.png'
+            ]);
         })
     );
 });
